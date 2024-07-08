d8ea89c8ff5a5714bdd6376313fa86a8d501cf2b
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-07-08 17:28:07 -0400

    feat: create navbar component

---
+++ shopping-cart/src/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return (
+        <>
+            <div className="nav">
+                <div className="logo">
+                    logo
+                </div>
+                <ul className="navbar">
+                    <li>Home</li>
+                    <li>Shop</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </>
+    );
+}
+ 
+export default Navbar;

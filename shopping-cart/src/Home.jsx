16d9ddf46addc628ea7cfef39b6390f5d3425143
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-07-09 11:08:57 -0400

    feat: create home component

---
+++ shopping-cart/src/Home.jsx
@@ -0,0 +1,12 @@
+import logo from './assets/logo.jpg'
+const Home = () => {
+    return (
+        <>
+            <div className="home">
+                <img className="home-logo" src={logo}></img>
+            </div>
+        </>
+    );
+}
+ 
+export default Home;

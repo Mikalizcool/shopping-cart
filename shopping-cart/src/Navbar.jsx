1dab5682f93fbdb7f9c6399b3028e7aeb6c34047
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-07-15 15:10:05 -0400

    feat: make navbar mobile responsive

--- shopping-cart/src/Navbar.jsx
+++ shopping-cart/src/Navbar.jsx
@@ -2,6 +2,15 @@ import logo from './assets/nav-logo.jpg'
 import {Link} from 'react-router-dom';
 import menu from './assets/menu.svg'
 const Navbar = ({total}) => {
+    const handleClick = () => {
+        let element = document.getElementById("mobile-navbar");
+        if (element.style.display === "none") {
+            element.style.display = "block";
+        }
+        else {
+            element.style.display = "none";
+        }
+    }
     return (
         <>
             <div className="nav">
@@ -13,7 +22,14 @@ const Navbar = ({total}) => {
                     <li><Link to="/shop">Shop</Link></li>
                     <li><Link to="/cart">Cart: {total}</Link></li>
                 </ul>
-                <button className="mobile-button"><img className="mobile-menu" src={menu}></img></button>
+                <div className="mobile-nav">
+                    <button onClick = {() => handleClick()}id="mobile-button"><img className="mobile-menu" src={menu}></img></button>
+                    <ul id="mobile-navbar">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/shop">Shop</Link></li>
+                        <li><Link to="/cart">Cart: {total}</Link></li>
+                    </ul>
+                </div>
             </div>
         </>
     );
